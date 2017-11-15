f7ad67c25f3970510a6ac88f746ec9b5f68ada71
Guillaume Milani <milani.guillaume@gmail.com>
2017-11-15 14:16:53 +0100

    Base project

---
+++ swagger/spring-server/src/main/java/io/lozzikit/survey/repositories/FruitRepository.java
@@ -0,0 +1,11 @@
+package io.lozzikit.survey.repositories;
+
+import io.lozzikit.survey.entities.FruitEntity;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by Olivier Liechti on 26/07/17.
+ */
+public interface FruitRepository extends CrudRepository<FruitEntity, Long>{
+
+}
