6ecd8e014c32b093781d6288541935f2ecf8029e
Guillaume Milani <milani.guillaume@gmail.com>
2018-01-17 10:26:06 +0100

    Route to new module

--- user_interface/src/app/layout/layout-routing.module.ts
+++ user_interface/src/app/layout/layout-routing.module.ts
@@ -15,7 +15,9 @@ const routes: Routes = [
             { path: 'bs-element', loadChildren: './bs-element/bs-element.module#BsElementModule' },
             { path: 'grid', loadChildren: './grid/grid.module#GridModule' },
             { path: 'components', loadChildren: './bs-component/bs-component.module#BsComponentModule' },
-            { path: 'blank-page', loadChildren: './blank-page/blank-page.module#BlankPageModule' }
+            { path: 'blank-page', loadChildren: './blank-page/blank-page.module#BlankPageModule' },
+            // Surveys
+            { path: 'surveys', loadChildren: './surveys/surveys.module#SurveysModule' },
         ]
     }
 ];
@@ -24,4 +26,4 @@ const routes: Routes = [
     imports: [RouterModule.forChild(routes)],
     exports: [RouterModule]
 })
-export class LayoutRoutingModule {}
+export class LayoutRoutingModule { }
