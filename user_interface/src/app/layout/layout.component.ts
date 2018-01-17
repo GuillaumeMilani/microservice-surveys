2724d0de57d9be169ea85f070dbdde1ad8aee5b8
Guillaume Milani <milani.guillaume@gmail.com>
2018-01-17 12:01:25 +0100

    Cleanup template2

--- user_interface/src/app/layout/layout.component.ts
+++ user_interface/src/app/layout/layout.component.ts
@@ -6,7 +6,33 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./layout.component.scss']
 })
 export class LayoutComponent implements OnInit {
-    constructor() {}
+    alerts: Array<any> = [];
 
-    ngOnInit() {}
+    constructor() {
+        this.alerts.push(
+            {
+                id: 1,
+                type: 'success',
+                message: `Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Voluptates est animi quibusdam praesentium quam, et perspiciatis,
+                    consectetur velit culpa molestias dignissimos
+                    voluptatum veritatis quod aliquam! Rerum placeat necessitatibus, vitae dolorum`
+            },
+            {
+                id: 2,
+                type: 'warning',
+                message: `Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Voluptates est animi quibusdam praesentium quam, et perspiciatis,
+                    consectetur velit culpa molestias dignissimos
+                    voluptatum veritatis quod aliquam! Rerum placeat necessitatibus, vitae dolorum`
+            }
+        );
+    }
+
+    ngOnInit() { }
+
+    public closeAlert(alert: any) {
+        const index: number = this.alerts.indexOf(alert);
+        this.alerts.splice(index, 1);
+    }
 }
--- user_interface/src/app/shared/index.ts
+++ user_interface/src/app/shared/index.ts
@@ -1,5 +1,4 @@
 export * from './modules';
 export * from './pipes/shared-pipes.module';
-export * from './modules';
 export * from './services';
 export * from './models';
