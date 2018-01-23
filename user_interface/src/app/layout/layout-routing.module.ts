c30e3a4a6bbd89bcb178d837678a77ed283938c1
Guillaume Milani <milani.guillaume@gmail.com>
2018-01-23 17:53:34 +0100

    Repaired routing

--- user_interface/src/app/layout/layout-routing.module.ts
+++ user_interface/src/app/layout/layout-routing.module.ts
@@ -7,7 +7,7 @@ const routes: Routes = [
         path: '',
         component: LayoutComponent,
         children: [
-            { path: '', redirectTo: '/surveys' },
+            { path: '', redirectTo: 'surveys' },
             { path: 'dashboard', loadChildren: './dashboard/dashboard.module#DashboardModule' },
             { path: 'charts', loadChildren: './charts/charts.module#ChartsModule' },
             { path: 'tables', loadChildren: './tables/tables.module#TablesModule' },
